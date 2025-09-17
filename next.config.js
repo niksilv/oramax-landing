b2d44f8c2578ec634772d21ad0d224ee49df9b8a
niksilv <219572028+niksilv@users.noreply.github.com>
2025-09-17 23:35:26 +0300

    chore: trigger vercel deploy

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 ﻿/** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
-    return [{ source: '/detector', destination: '/detector/index.html' }];
+    return [
+      { source: '/detector',  destination: '/detector/index.html' },
+      { source: '/detector/', destination: '/detector/index.html' },
+      { source: '/detector/api/:path*', destination: 'http://localhost:8000/exoplanet/:path*' },
+    ];
+  },
+  async headers() {
+    return [{ source: '/:path*', headers: [{ key: 'Cache-Control', value: 'no-store' }]}];
   },
 };
 module.exports = nextConfig;
