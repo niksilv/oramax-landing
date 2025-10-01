7d66d28ce8187e9b59e9976ae0cda7ae53951335
niksilv <219572028+niksilv@users.noreply.github.com>
2025-10-01 22:19:28 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,22 @@
 // app/layout.tsx
 import type { Metadata } from "next";
-import "./globals.css"; // ΠΡΕΠΕΙ να υπάρχει αυτό το import
-
+import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
   title: "Orama X",
-  description: "Exoplanet Detector",
+  description: "Exoplanet Detector & Project",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="h-full">
-      <body className="min-h-screen flex flex-col bg-black text-slate-100">
-        {/* HEADER */}
+      <body className="min-h-screen flex flex-col bg-white text-slate-900">
         <Header />
-
-        {/* MAIN */}
         <main className="flex-1">
           <div className="max-w-6xl mx-auto px-4">{children}</div>
         </main>
-
-        {/* FOOTER */}
         <Footer />
       </body>
     </html>
