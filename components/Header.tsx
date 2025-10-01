fcdbdb91c5b958f1e282b0eaec3b895f2a61b1f8
niksilv <219572028+niksilv@users.noreply.github.com>
2025-10-01 21:25:09 +0300

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,11 @@
+// components/Header.tsx
 import React from "react";
 import Navbar from "./Navbar";
 
 export default function Header() {
   return (
     <header className="w-full border-b border-white/10">
-      <div className="max-w-6xl mx-auto px-4 py-3">
+      <div className="max-w-6xl mx-auto px-4 py-4">
         <Navbar />
       </div>
     </header>
