3ef6dece47f4384f22575fd7821c31953dc35919
niksilv <219572028+niksilv@users.noreply.github.com>
2025-10-01 23:10:16 +0300

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="border-t border-black/10">
+    <footer className="border-t border-black/0">
       <div className="max-w-6xl mx-auto px-4 py-6 text-center text-sm text-slate-600">
         © {new Date().getFullYear()} Orama X — All rights reserved.
       </div>
