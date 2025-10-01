d79c5674bcdfb3823b1ca3cec2dcd46c6ff96c46
niksilv <219572028+niksilv@users.noreply.github.com>
2025-10-01 20:52:27 +0300

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,1 +1,13 @@
-﻿export { default } from "./Navbar";
+import React from "react";
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="flex items-center gap-5">
+      <Link href="/" className="hover:opacity-80">Home</Link>
+      <Link href="/our-project/our-challenge" className="hover:opacity-80">Our Challenge</Link>
+      <Link href="/contact-us" className="hover:opacity-80">Contact</Link>
+      <Link href="/detector" className="hover:opacity-80">Detector</Link>
+    </nav>
+  );
+}
